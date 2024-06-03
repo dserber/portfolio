e8ca33a8da483e4b8202c7a6784ffe21375f039d
David Serber <dserber@gmail.com>
2024-06-03 13:01:27 -0500

    Fixed bug with header rendering

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,12 @@
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Header() {
+  const [activeSection, setActiveState] = useState("home");
+
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -16,12 +20,17 @@ export default function Header() {
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
-              className="h-3/4 flex items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+              className="h-3/4 flex items-center justify-center"
               key={link.hash}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
-              <Link href={link.hash}>{link.name}</Link>
+              <Link
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                href={link.hash}
+              >
+                {link.name}
+              </Link>
             </motion.li>
           ))}
         </ul>
