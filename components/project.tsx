2b7b24834f43eb07a2258af7ef4235d2f4e4704e
David Serber <dserber@gmail.com>
2024-06-03 12:54:14 -0500

    Added navigation to various sections from the navbar. Enforced smooth scrolling

--- components/project.tsx
+++ components/project.tsx
@@ -51,21 +51,7 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
-        transition 
-        group-hover:scale-[1.04] 
-    
-        group-hover:-translate-x-3 
-        group-hover:translate-y-3 
-        group-hover:-rotate-2 
-    
-        group-even:group-hover:translate-x-3 
-        group-even:group-hover:translate-y-3 
-        group-even:group-hover:rotate-2
-        
-        group-even:-left-40 
-        group-even:right-[initial]
-        "
+          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:-left-40 group-even:right-[initial]"
         />
       </section>
     </motion.div>
