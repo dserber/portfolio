582d4e3ba9e5e51ac56067aac47c2ecec32537cb
David Serber <dserber@gmail.com>
2024-06-06 12:16:05 -0500

    Added react email package. Styled contact form email in separate file

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { getErrorMessage, validateString } from "@/lib/utils";
-import { error } from "console";
 import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -28,7 +29,11 @@ export const sendEmail = async (formData: FormData) => {
       to: "dserber@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
-      text: message as string,
+      // text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
     return {
