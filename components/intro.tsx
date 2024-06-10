ee1fdc895e1943a8850260850e7baf533a03ce80
David Serber <dserber@gmail.com>
2024-06-10 13:06:48 -0500

    Added headshot

--- components/intro.tsx
+++ components/intro.tsx
@@ -30,7 +30,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src="/headshot.jpg"
               alt="David Portrait"
               height="192"
               width="192"
