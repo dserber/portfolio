a84dff682170fdcb559db82ff8d4e54fb5c614a1
David Serber <dserber@gmail.com>
2024-06-11 18:06:10 -0500

    Small change to wording in intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -60,7 +60,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm David.</span> I'm a{" "}
-        <span className="font-bold">front-end web & mobile developer</span> with{" "}
+        <span className="font-bold">web & mobile developer</span> with{" "}
         <span className="font-bold">6 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js) & Flutter</span>.
