ea4d142acbb0eaba7d757995327350e497bb97ad
David Serber <dserber@gmail.com>
2024-06-15 01:20:47 -0500

    Resetting the contact form after being sent successfully

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
@@ -10,6 +10,7 @@ import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+  const form = useRef<HTMLFormElement>(null);
 
   return (
     <motion.section
@@ -34,6 +35,7 @@ export default function Contact() {
         For inquiries, please contact me through this form.
       </p>
       <form
+        ref={form}
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
@@ -42,6 +44,7 @@ export default function Contact() {
             toast.error(error);
             return;
           }
+          form.current?.reset();
           toast.success("Email sent successfully!");
         }}
       >
